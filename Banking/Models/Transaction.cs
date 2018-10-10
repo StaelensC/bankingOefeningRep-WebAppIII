628235199f7a468370045cf74ea481062ba6f4f7
Cedric Staelens <cedric.staelens@student.hogent.be>
2018-10-10 17:38:59 +0200

    Maak Transaction en TransactionType klassen aan

---
+++ Banking/Models/Transaction.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Banking.Models
+{
+    class Transaction
+    {
+
+        #region Properties
+        public decimal Amount { get; private set; }
+        public DateTime DateOfTrans { get; private set; }
+        public TransactionType TransactionType { get; private set; }
+        #endregion
+
+        #region Constructors
+        public Transaction(decimal amount, TransactionType type)
+        {
+            TransactionType = type;
+            Amount = amount;
+            DateOfTrans = DateTime.Today;
+        }
+        #endregion
+
+        #region Methods
+        public bool IsDeposit { get => TransactionType == TransactionType.Deposit; }
+        public bool IsWithdraw { get => TransactionType == TransactionType.Withdraw; }
+        #endregion
+    }
+}
